d8957161e0289a367d8502918954e380aa992ebb
DoomWave <acunanajair9@gmail.com>
2024-10-03 14:50:28 -0700

    configuring temperature.js

--- utils/temperature.js
+++ utils/temperature.js
@@ -1,3 +1,19 @@
 const UNITS = {
-    
+    celsius : "°C",
+    fahrenheit: "°F"
 }
+
+function convertTemperatureTo(temperature, unitTo){
+    if(unitTo === UNITS.celsius){
+        return(temperature - 32)/ 1.
+    }else if (unitTo === UNITS.fahrenheit){
+        return temperature * 1.8 + 32;
+    }else{
+      throw new Error("Invalid unit");
+    }
+}
+
+function getOppositeUnit(unit){
+    return unit=== UNITS.celsius ? UNITS.fahrenheit : UNITS.celsius;
+}
+export { UNITS, convertTemperatureTo }
