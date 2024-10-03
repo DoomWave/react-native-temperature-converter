19d53680cc249b2454ce401deddab3ce91ef1515
DoomWave <acunanajair9@gmail.com>
2024-10-02 20:19:54 -0700

    still trying.

--- components/input/Input.jsx
+++ components/input/Input.jsx
@@ -1,7 +1,10 @@
-import {View, TextInput } from "react-native";
+import {View, TextInput, Text } from "react-native";
 import { style } from "./input.style";
 export function Input() {
     return (
+    <View style={s.root}>
     <TextInput style={style.input} placeholder="Type your temperature"/>
-);
+    <Text style={s.unit}>°C</Text>
+    </View>
+    )
 }
--- components/input/Input.style.js
+++ components/input/Input.style.js
@@ -1,11 +1,16 @@
 import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
+  root: {
+    alignSelf: "stretch",
+  },
   input: {
     backgroundColor: "white",
     height: 50,
     borderRadius: 20,
-    alignSelf: "stretch",
     paddingLeft: 25,
     },
+  unit: {
+    position: "absolute"
+  }
 });
