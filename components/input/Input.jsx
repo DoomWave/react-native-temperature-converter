cad8e1415abf80c762e633c8040a90155da80932
DoomWave <acunanajair9@gmail.com>
2024-10-02 20:09:46 -0700

    Fixed.

--- components/input/Input.jsx
+++ components/input/Input.jsx
@@ -1,7 +1,7 @@
 import {View, TextInput } from "react-native";
 import { style } from "./input.style";
-export function Input(){
+export function Input() {
     return (
     <TextInput style={style.input} placeholder="Type your temperature"/>
-)
+);
 }
