f12f912466259bb5ca9b8ea3c49c246059f886d4
DoomWave <acunanajair9@gmail.com>
2024-10-02 20:41:12 -0700

    inputValue and setInputValue added.

--- components/input/Input.jsx
+++ components/input/Input.jsx
@@ -1,13 +1,16 @@
 import {View, TextInput, Text } from "react-native";
 import { s } from "./Input.style";
-export function Input({defaultValue}) {
+export function Input({defaultValue, onChange}) {
     return (
     <View style={s.root}>
     <TextInput 
     style={s.Input} 
     maxLength={3} 
     placeholder="Type your temperature"
-    defaultValue={defaultValue}
+    defaultValue={defaultValue.toString()}
+    onChangeText={ (text) => {
+        onChange(text);
+    }}
     />
     <Text style={s.unit}>°C</Text>
     </View>
